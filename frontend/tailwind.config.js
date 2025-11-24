64d8504a6cd5515ca4d2ca573eecb7f40dfc742f
yashnawgiri45 <yashnawgiri45@gmail.com>
2025-11-24 07:05:36 +0000

    Changes before Firebase Studio auto-run

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  darkMode: 'class', // This is the crucial change
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
